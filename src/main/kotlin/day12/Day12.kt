fb579fb22d87ca6e9303a1bafe19946c31af2e4b
fabmax <fabmax.thiele@gmail.com>
2023-12-12 22:49:18 +0100

    day 12 - rewrote recursion from scratch

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -10,177 +10,54 @@ class Day12 : AocPuzzle() {
     override val answer2 = 280382734828319L
 
     override fun solve(input: List<String>): Pair<Any?, Any?> {
-        val patterns = input.map { line ->
-            val (pattern, shape) = line.split(" ")
-            val counts = shape.split(",").map { it.toInt() }
-            pattern to counts
+        val conditionsAndGroups = input.map { line ->
+            val (conditions, groups) = line.split(" ")
+            val counts = groups.split(",").map { it.toInt() }
+            conditions to counts
         }
 
-        val answer1 = patterns.sumOf { (p, s) ->
-            //matchBruteForce(p, s).size
-            matchRecursive(p, s)
+        val answer1 = conditionsAndGroups.sumOf { (conditions, groups) ->
+            countArrangements(conditions, groups)
         }
 
-        val answer2 = patterns.sumOf { (p, s) ->
-            val expP = "$p?$p?$p?$p?$p"
-            val expS = s + s + s + s + s
-            matchRecursive(expP, expS)
+        val answer2 = conditionsAndGroups.sumOf { (conds, grps) ->
+            val conditions = (1..5).joinToString(separator = "?") { conds }
+            val groups = (1..5).flatMap { grps }
+            countArrangements(conditions, groups)
         }
 
         return answer1 to answer2
     }
 
-    /**
-     * Fancy (memoized) recursive method: Wouldn't have come up with that on my own without browsing through several
-     * discussions and tips.
-     */
-    private fun matchRecursive(pattern: String, shape: List<Int>): Long {
-        val resultCache = mutableMapOf<MatchState, Long>()
+    private fun countArrangements(conditions: String, groups: List<Int>): Long {
+        val cache = mutableMapOf<Pair<Int, Int>, Long>()
 
-        fun memoize(state: MatchState): Long {
-            resultCache[state]?.let { return it }
+        fun recurse(charPos: Int, groupPos: Int): Long {
+            cache[charPos to groupPos]?.let { return it }
+            var count = 0L
 
-            val requiredGroupSize = shape.getOrElse(state.groupIndex) { 0 }
-            val result = when {
-                // terminal checks: either successfully matched (return 1) or not (return 0)
-                state.groupIndex > shape.size -> 0L
-                state.groupProgress < requiredGroupSize && !state.isInsideGroup -> 0L
-                state.groupProgress > requiredGroupSize -> 0L
-                state.nextCharIndex == pattern.length -> {
-                    if (state.groupProgress == requiredGroupSize && state.groupIndex == shape.size - 1) 1L else 0L
-                }
-
-                // continue with parsing / matching
-                else -> when (val c = pattern[state.nextCharIndex]) {
-                    '.' -> memoize(state.advance(nextInsideGroup = false))
-                    '#' -> memoize(state.advance(nextInsideGroup = true))
-                    '?' -> memoize(state.advance(nextInsideGroup = false)) +
-                           memoize(state.advance(nextInsideGroup = true))
-
-                    else -> error("Unexpected pattern char: $c")
-                }
-            }
-
-            return result.also { resultCache[state] = it }
-        }
-
-        return memoize(MatchState())
-    }
-
-    data class MatchState(
-        val nextCharIndex: Int = 0,
-        val groupIndex: Int = -1,
-        val groupProgress: Int = 0,
-        val isInsideGroup: Boolean = false
-    ) {
-        fun advance(nextInsideGroup: Boolean): MatchState {
-            val nextGroupIndex = when {
-                !isInsideGroup && nextInsideGroup -> groupIndex + 1     // entering new group
-                else -> groupIndex
-            }
-            val nextGroupProgress = when {
-                !isInsideGroup && nextInsideGroup -> 1                  // entering new group
-                nextInsideGroup -> groupProgress + 1                    // progress in group
-                else -> groupProgress                                   // leaving group or outside group
-            }
-            return copy(
-                nextCharIndex = nextCharIndex + 1,
-                groupIndex = nextGroupIndex,
-                groupProgress = nextGroupProgress,
-                isInsideGroup = nextInsideGroup
-            )
-        }
-    }
-
-    /**
-     * Not used anymore, only here for melancholic reasons: My initial terrible part 1 solution.
-     */
-    private fun matchBruteForce(pattern: String, shape: List<Int>): Set<String> {
-        val requiredPlaces = shape.sum()
-        val maxPlaces = pattern.count { c -> c == '?' || c == '#' }
-        val numWildcards = pattern.count { c -> c == '?' }
-        val numOptions = (1 shl numWildcards)
-
-        val fittedPatterns = mutableSetOf<String>()
-
-        for (mask in 0 ..< numOptions) {
-            val numOnes = mask.countOneBits()
-            if (maxPlaces - numOnes < requiredPlaces) {
-                continue
-            }
-
-            var replaceI = 0
-            val testPattern = pattern.map {
-                if (it == '?') {
-                    if (mask and (1 shl replaceI++) != 0) '.' else '?'
-                } else {
-                    it
-                }
-            }.joinToString("")
-
-            matchGreedy(testPattern, shape)?.let {
-                check(it.length == pattern.length)
-                fittedPatterns += it
-            }
-        }
-
-        return fittedPatterns
-    }
-
-    private fun matchGreedy(pattern: String, shape: List<Int>): String? {
-        var shapeI = 0
-        var patternPos = 0
-        var matched = ""
-
-        while (shapeI < shape.size) {
-            if (patternPos >= pattern.length) {
-                return null
-            }
-
-            val count = shape[shapeI]
-            val leadingSpace = pattern.substring(patternPos).takeWhile { it == '.' }
-            patternPos += leadingSpace.length
-            matched += leadingSpace
-
-            val group = pattern.substring(patternPos).takeWhile { it == '#' || it == '?' }
-            if (group.length >= count) {
-                if (group.count { it == '#' || it == '?' } >= count && (group.length == count || group[count] == '?')) {
-                    shapeI++
-                    patternPos += count
-                    repeat(count) { matched += "#" }
-
-                    if (patternPos < pattern.length) {
-                        patternPos++
-                        matched += "."
-                    }
-
-                } else if (group[0] == '?') {
-                    patternPos++
-                    matched += '.'
-
-                } else {
-                    // group starts with unmatched '#'
-                    return null
-                }
-
-            } else if (group.all { it == '?' }) {
-                repeat(group.length) { matched += "." }
-                patternPos += group.length
+            if (groupPos == groups.size) {
+                count = if ((charPos..conditions.lastIndex).none { conditions[it] == '#' }) 1L else 0L
 
             } else {
-                // group contains unmatched '#'
-                return null
+                val groupSize = groups[groupPos]
+                for (i in charPos .. conditions.length - groupSize) {
+                    val groupEnd = i + groupSize
+                    val isGroupFit = (i until groupEnd).all { conditions[it] != '.' }
+                    val isDelimited = (conditions.length == groupEnd || conditions[groupEnd] != '#')
+
+                    if (isGroupFit && isDelimited) {
+                        count += recurse(groupEnd + 1, groupPos + 1)
+                    }
+                    if (conditions[i] == '#') {
+                        break
+                    }
+                }
             }
+            return count.also { cache[charPos to groupPos] = it }
         }
 
-        if (pattern.length > patternPos && pattern.substring(patternPos).any { it == '#' }) {
-            // remaining pattern has unmatched '#'
-            return null
-        } else {
-            repeat(pattern.length - patternPos) { matched += '.' }
-        }
-
-        return matched
+        return recurse(0, 0)
     }
 
     init {
