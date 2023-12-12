01f7b73c3362cf36e1f799c99c7c413c38e0c37e
fabmax <fabmax.thiele@gmail.com>
2023-12-12 23:31:10 +0100

    day 12 - minor performance improvement

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -31,20 +31,19 @@ class Day12 : AocPuzzle() {
 
     private fun countArrangements(conditions: String, groups: List<Int>): Long {
         val cache = mutableMapOf<Pair<Int, Int>, Long>()
-
         fun recurse(charPos: Int, groupPos: Int): Long {
             cache[charPos to groupPos]?.let { return it }
-            var count = 0L
 
+            var count = 0L
             if (groupPos == groups.size) {
                 count = if ((charPos..conditions.lastIndex).none { conditions[it] == '#' }) 1L else 0L
 
             } else {
-                val groupSize = groups[groupPos]
-                for (i in charPos .. conditions.length - groupSize) {
-                    val groupEnd = i + groupSize
+                val minRequiredLen = (groupPos..groups.lastIndex).sumOf { groups[it] } + groups.lastIndex - groupPos
+                for (i in charPos .. conditions.length - minRequiredLen) {
+                    val groupEnd = i + groups[groupPos]
                     val isGroupFit = (i until groupEnd).all { conditions[it] != '.' }
-                    val isDelimited = (conditions.length == groupEnd || conditions[groupEnd] != '#')
+                    val isDelimited = conditions.length == groupEnd || conditions[groupEnd] != '#'
 
                     if (isGroupFit && isDelimited) {
                         count += recurse(groupEnd + 1, groupPos + 1)
