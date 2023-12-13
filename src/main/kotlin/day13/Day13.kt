7d0d2e9531fe8bd4c9f88e82696a15f7f481e560
Max Thiele <fabian.thiele@fokus.fraunhofer.de>
2023-12-13 11:11:45 +0100

    day13

---
+++ src/main/kotlin/day13/Day13.kt
@@ -0,0 +1,79 @@
+package day13
+
+import AocPuzzle
+import kotlin.math.min
+
+fun main() = Day13().start()
+
+class Day13 : AocPuzzle() {
+
+    override val answer1 = 27502
+    override val answer2 = 31947
+
+    override fun solve(input: List<String>): Pair<Any?, Any?> {
+        val patterns = input.fold(mutableListOf(mutableListOf<String>())) { groups, line ->
+            groups.apply {
+                when {
+                    line.isBlank() -> add(mutableListOf())
+                    else -> last().add(line)
+                }
+            }
+        }.map { Pattern(it) }
+
+        val answer1 = patterns.sumOf {
+            100 * it.findReflectionCenterRow(0) + it.findReflectionCenterCol(0)
+        }
+        val answer2 = patterns.sumOf {
+            100 * it.findReflectionCenterRow(1) + it.findReflectionCenterCol(1)
+        }
+        return answer1 to answer2
+    }
+
+    class Pattern(val lines: List<String>) {
+
+        private val transposed = lines[0].indices.map { col ->
+            lines.map { it[col] }.joinToString(separator = "")
+        }
+
+        fun findReflectionCenterRow(smudgeCount: Int): Int {
+            return (1 .. lines.lastIndex).firstOrNull { lines.isReflectingAt(it, smudgeCount) } ?: 0
+        }
+
+        fun findReflectionCenterCol(smudgeCount: Int): Int {
+            return (1 .. transposed.lastIndex).firstOrNull { transposed.isReflectingAt(it, smudgeCount) } ?: 0
+        }
+
+        private fun List<String>.isReflectingAt(index: Int, smudgeCount: Int): Boolean {
+            val iterations = min(index, lastIndex - index + 1)
+            return smudgeCount == (0 ..< iterations).sumOf {
+                get(index - it - 1).distance(get(index + it))
+            }
+        }
+
+        private fun String.distance(b: String): Int = indices.map { if (this[it] == b[it]) 0 else 1 }.sum()
+    }
+
+    init {
+        testInput(
+            text = """
+                #.##..##.
+                ..#.##.#.
+                ##......#
+                ##......#
+                ..#.##.#.
+                ..##..##.
+                #.#.##.#.
+
+                #...##..#
+                #....#..#
+                ..##..###
+                #####.##.
+                #####.##.
+                ..##..###
+                #....#..#
+            """.trimIndent(),
+            expected1 = 405,
+            expected2 = 400
+        )
+    }
+}
