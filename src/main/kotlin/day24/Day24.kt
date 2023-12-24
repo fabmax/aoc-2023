f6a15a4e20fb3fb48a17c4742b6a244e107958a2
fabmax <fabmax.thiele@gmail.com>
2023-12-24 22:06:46 +0100

    day24 - fully working solution

--- src/main/kotlin/day24/Day24.kt
+++ src/main/kotlin/day24/Day24.kt
@@ -4,9 +4,8 @@ import AocPuzzle
 import de.fabmax.kool.math.MutableVec3d
 import de.fabmax.kool.math.Vec3d
 import kotlin.math.abs
-import kotlin.math.roundToLong
 
-fun main() = Day24.runPuzzle()
+fun main() = Day24.runAll()
 
 object Day24 : AocPuzzle<Int, Long>() {
 
@@ -46,50 +45,67 @@ object Day24 : AocPuzzle<Int, Long>() {
             it.split(" @ ").let { (p, v) -> HailStone(Vec3d(p), Vec3d(v)) }
         }
 
-        val (ha, hb) = hailStones.take(2)
-        val aSamples = ha.sample(0, 1e12.toLong(), 500)
-        val bSamples = hb.sample(0, 1e12.toLong(), 500)
-        var minDist = Double.POSITIVE_INFINITY
-        var bestA = 0
-        var bestB = 0
+        val (h1, h2) = hailStones.take(2)
+        var t1 = 0L
+        var t2 = 0L
 
-        for (i in aSamples.indices) {
-            for (j in bSamples.indices) {
-                val a = aSamples[i].first
-                val b = bSamples[j].first
-                var sumDist = 0.0
-                for (k in 2 until hailStones.size) {
-                    sumDist += hailStones[k].distanceToLine(a, b)
+        var step1 = if (isTestRun()) 1 else 1e10.toLong()
+        var step2 = if (isTestRun()) 1 else 1e10.toLong()
+        var isStepA = true
+
+        // approximate collision times 1 and 2
+        var prevDist: Double
+        var dist = Double.POSITIVE_INFINITY
+        while (abs(step1) != 0L || abs(step2) != 0L) {
+            do {
+                prevDist = dist
+                when (isStepA) {
+                    true  -> t1 += step1
+                    false -> t2 += step2
                 }
-                if (sumDist < minDist) {
-                    minDist = sumDist
-                    bestA = i
-                    bestB = j
+                val a = h1.at(t1)
+                val b = h2.at(t2)
+                dist = hailStones.eval(a, b)
+            } while (dist < prevDist)
+
+            when (isStepA) {
+                true  -> step1 /= -10
+                false -> step2 /= -10
+            }
+            isStepA = !isStepA
+        }
+
+        // approximated collision times are 1 off -> do final refinement
+        var bestDist = dist
+        for (s1 in t1 - 5L .. t1 + 5L) {
+            for (s2 in t2 - 5L .. t2 + 5L) {
+                val a = h1.at(s1)
+                val b = h2.at(s2)
+                val d = hailStones.eval(a, b)
+                if (d < bestDist) {
+                    bestDist = d
+                    t1 = s1
+                    t2 = s2
                 }
             }
         }
 
-        val dt = bSamples[bestB].second - aSamples[bestA].second
-        val dp = bSamples[bestB].first - aSamples[bestA].first
-        val v = dp / dt.toDouble()
-        val vx = v.x.roundToLong().toDouble()
-        val vy = v.y.roundToLong().toDouble()
-        val vz = v.z.roundToLong().toDouble()
-        println("estimated v: $vx, $vy, $vz")
+        val hailPos1 = h1.at(t1)
+        val hailPos2 = h2.at(t2)
 
-        val (h1, h2) = hailStones.take(2)
+        val vx = (hailPos2.x - hailPos1.x).toLong() / (t2 - t1)
+        val vy = (hailPos2.y - hailPos1.y).toLong() / (t2 - t1)
+        val vz = (hailPos2.z - hailPos1.z).toLong() / (t2 - t1)
 
-        // copy and paste output to wolfram alpha...
-        println("${h1.pos.x.toLong()}+${h1.vel.x.toLong()}*s=x+${vx.toLong()}*s,")
-        println("${h1.pos.y.toLong()}+${h1.vel.y.toLong()}*s=y+${vy.toLong()}*s,")
-        println("${h1.pos.z.toLong()}+${h1.vel.z.toLong()}*s=z+${vz.toLong()}*s,")
-        println("${h2.pos.x.toLong()}+${h2.vel.x.toLong()}*t=x+${vx.toLong()}*t,")
-        println("${h2.pos.y.toLong()}+${h2.vel.y.toLong()}*t=y+${vy.toLong()}*t,")
-        println("${h2.pos.z.toLong()}+${h2.vel.z.toLong()}*t=z+${vz.toLong()}*t")
+        val px = hailPos1.x.toLong() - vx * t1
+        val py = hailPos1.y.toLong() - vy * t1
+        val pz = hailPos1.z.toLong() - vz * t1
 
-        // wolfram alpha says:
-        // s = 353090968659, t = 870093641616, x = 194723518367339, y = 181910661443432, z = 150675954587450
-        return 194723518367339 + 181910661443432 + 150675954587450
+        return px + py + pz
+    }
+
+    fun List<HailStone>.eval(a: Vec3d, b: Vec3d): Double = sumOf {
+        it.distanceToLine(a, b)
     }
 
     fun Vec3d(str: String): Vec3d {
@@ -114,20 +130,10 @@ object Day24 : AocPuzzle<Int, Long>() {
         fun at(t: Long) = pos + vel * t.toDouble()
 
         fun distanceToLine(l1: Vec3d, l2: Vec3d): Double {
-            val r1 = pos
-            val r2 = l1
-            val e1 = vel
-            val e2 = l2 - l1
-
+            val e1 = l2 - l1
+            val e2 = vel
             val n: Vec3d = e1.cross(e2, MutableVec3d())
-            return abs(n dot (r1 - r2)) / n.length()
-        }
-
-        fun sample(fromT: Long, toT: Long, nSamples: Int): List<Pair<Vec3d, Long>> {
-            val tStep = (toT - fromT) / (nSamples - 1)
-            return (0 until nSamples).map { i ->
-                at(fromT + tStep * i) to tStep * i
-            }
+            return abs(n dot (l1 - pos)) / n.length()
         }
     }
 }
